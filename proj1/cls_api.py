93ceacc131e72513250ed9417c48295c199de1ad
DESKTOP-Q1OMGL4\201 <usialeta@gmail.com>
2024-12-31 12:05:45 +0900

    commit

---
+++ proj1/cls_api.py
@@ -0,0 +1,60 @@
+# STEP 1: Import the necessary modules.
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python.components import processors
+from mediapipe.tasks.python import vision
+
+# STEP 2: Create an ImageClassifier object.
+base_options = python.BaseOptions(model_asset_path='models\\efficientnet_lite0.tflite')
+options = vision.ImageClassifierOptions(base_options=base_options, max_results=3)
+classifier = vision.ImageClassifier.create_from_options(options)
+
+from fastapi import FastAPI, UploadFile
+
+import cv2
+import numpy as np
+app = FastAPI()
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    # STEP 3: Load the input image.
+
+    contents = await file.read()
+    nparr = np.fromstring(contents, np.uint8)    
+    # cv_mat = cv2.imread(input_file) # file open + image decode
+    cv_mat = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    image = mp.Image(image_format= mp.ImageFormat.SRGB, data = cv_mat)
+
+    # STEP 4: Classify the input image.
+    classification_result = classifier.classify(image)
+
+    # STEP 5: Process the classification result. In this case, visualize it.
+    top_category = classification_result.classifications[0].categories[0]
+    result = (f"{top_category.category_name} ({top_category.score:.2F})")
+
+
+    return {"result": result}
+
+
+import cv2
+import numpy as np
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    # STEP 3: Load the input image.
+    # open file -> binary
+    # http file -> text -> binary
+    contents = await file.read()
+    nparr = np.fromstring(contents, np.uint8)
+    cv_mat = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv_mat)
+    
+    # STEP 4: Classify the input image.
+    # image = mp.Image.create_from_file('burger.jpg')
+    classification_result = classifier.classify(image)
+    # print(classification_result)
+    
+    # STEP 5: Process the classification result. In this case, visualize it.
+    top_category = classification_result.classifications[0].categories[0]
+    result = f"{top_category.category_name} ({top_category.score:.2f})"
+    return {"result": result}
---
+++ proj1/data.py
@@ -0,0 +1,10 @@
+DetectionResult(
+    detections=[
+        Detection(
+            bounding_box=BoundingBox(origin_x=134, origin_y=26, width=94, height=133),
+            categories=[Category(index=None, score=0.6206778287887573, display_name=None, category_name='dog')], keypoints=[]), 
+        Detection(
+            bounding_box=BoundingBox(origin_x=72, origin_y=24, width=83, height=136),
+            categories=[Category(index=None, score=0.541317343711853, display_name=None, category_name='dog')], keypoints=[])
+        ]
+    )
