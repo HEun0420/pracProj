1845cfbec205fbbe770b7dc12153f3888e20dd7e
DESKTOP-Q1OMGL4\201 <usialeta@gmail.com>
2025-01-13 11:56:01 +0900

    commit

---
+++ proj5/api.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, UploadFile
+
+app = FastAPI()
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    
+    contents = await file.read()
+    
+    return {"filename": file.filename,
+            "filesize": len(contents)}
